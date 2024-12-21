89ffc22f79dc1851e1dc1b2523153e7af78802ad
brnlf1990 <brnlf1990@gmail.com>
2024-12-21 09:29:25 +0000

    add log

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const usersRouter = require("./routes/users");
 const costsRouter = require("./routes/costs");
 const revenueRouter = require("./routes/revenue");
 const auth = require("./middleware/auth")
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 const { login, addUsers } = require("./controlers/users");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const { requestLogger, errorLogger } = require('./middleware/logger');
 const app = express();
 const allowedCors = [
    'http://localhost:3000',
@@ -15,11 +16,14 @@ const allowedCors = [
 mongoose.connect("mongodb://localhost:27017/financialdb");
 app.use(express.json());
 app.use(cors({
+
   origin:allowedCors,
   methods:['GET', 'HEAD', 'POST', 'DELETE', 'PATCH', 'PUT'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
 }))
+app.use(requestLogger);
+
 app.post("/signin", login);
 app.post("/signup", addUsers);
 
@@ -28,7 +32,7 @@ app.use("/users", usersRouter);
 app.use("/revenue", revenueRouter);  
 app.use("/costs", costsRouter);
 
-
+app.use(errorLogger);
 app.use((err, req, res, next) => {
   if (!err.status){
     res.status(500).send({message:err.message})
